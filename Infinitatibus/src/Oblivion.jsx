ac829e8272ab231f32b7c6aae1a22b493badce90
Rose, Ian Christian <ianrose@iu.edu>
2026-05-18 23:38:46 -0400

    setup

---
+++ Infinitatibus/src/Oblivion.jsx
@@ -0,0 +1,9 @@
+
+export default function Oblivion() {
+
+  return (
+    <>
+      What?
+    </>
+  );
+}
---
+++ Infinitatibus/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './Home.jsx'
+import Test from './Test.jsx';
+import Oblivion from './Oblivion.jsx';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/test" element={<Test/>}/>
+        <Route path="*" element={<Oblivion />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
